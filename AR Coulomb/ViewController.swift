1b4c9159bf6fc951cf303cf919bd850a0017a909
leomav <leo.mav.8@gmail.com>
2020-06-25 18:27:21 +0300

    (1)Changed the .rcproject to only having point charge object. Text Entity is now added programmatically through 2 functions, by loading the mesh and material of its ModelComponent. Text Entity disappears when PointCharge is moving. (2) Replaced the Point TapRecognizer with a Point LongPressRecognizer. (3) Added scaling up and down the pointCharge for emphasis.

--- AR Coulomb/ViewController.swift
+++ AR Coulomb/ViewController.swift
@@ -56,6 +56,19 @@ class ViewController: UIViewController {
             ]
     ]
     
+//    var simpleMaterial = SimpleMaterial()
+//    simpleMaterial.metallic = MaterialScalarParameter(floatLiteral: 0.2)
+//    simpleMaterial.roughness = MaterialScalarParameter(floatLiteral: 0.1)
+//    simpleMaterial.tintColor = UIColor.white
+    
+    let coulombTextMaterial: SimpleMaterial = {
+        var mat = SimpleMaterial()
+        mat.metallic = MaterialScalarParameter(floatLiteral: 0.2)
+        mat.roughness = MaterialScalarParameter(floatLiteral: 0.1)
+        mat.tintColor = UIColor.white
+        return mat
+    }()
+    
     var selectedPositions: [SIMD3<Float>] = []
     
     var startingWorldPosition: SIMD3<Float> = SIMD3<Float>(0, 0, 0)
@@ -100,12 +113,13 @@ class ViewController: UIViewController {
         firstPointTapRecognizer.name = "First Point Recognizer"
         arView.addGestureRecognizer(firstPointTapRecognizer)
         
-        // Tap Recognizer for pointChargeEntities
-        // Don't enable it, until the first point Charges are added
-        let pointTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(handlePointTap(recognizer:)))
-        pointTapRecognizer.name = "Point Tap Recognizer"
-        arView.addGestureRecognizer(pointTapRecognizer)
-        pointTapRecognizer.isEnabled = false
+        // Long Press Recognizer to enable parameters interaction with Point Charge (min press 1 sec)
+        let longPressRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress(recognizer:)))
+        longPressRecognizer.name = "Long Press Recognizer"
+        longPressRecognizer.minimumPressDuration = 1
+        arView.addGestureRecognizer(longPressRecognizer)
+        longPressRecognizer.isEnabled =  false
+        
     }
     
     func setupARView() {
@@ -115,31 +129,6 @@ class ViewController: UIViewController {
         config.environmentTexturing = .automatic
         arView.session.run(config)
 
-    }
-    
-    @objc func handleTap(recognizer: UITapGestureRecognizer) {
-        let location = recognizer.location(in: arView)
-        
-        let results = arView.raycast(from: location, allowing: .estimatedPlane, alignment: .horizontal)
-        
-        if let firstResult = results.first {
-            let anchor = ARAnchor(name: "PointCharge", transform: firstResult.worldTransform)
-            print(firstResult.worldTransform)
-            arView.session.add(anchor: anchor)
-        } else {
-            print("No horizontal surface found.")
-        }
-    }
-    
-    @objc func handlePointTap(recognizer: UITapGestureRecognizer) {
-        let location = recognizer.location(in: arView)
-        
-        guard let hitEntity = arView.entity(at: location) else {return}
-        
-        if hitEntity.name == "pointChargeObject" {
-            tappedEntity = hitEntity
-            loadPointChargeInfo(entity: tappedEntity)
-        }
     }
 
     func addStackView() {
@@ -185,7 +174,6 @@ class ViewController: UIViewController {
     }
     
     
-    
     func placeObject(for anchor: ARAnchor) {
         // Add the anchor of the scene where the user tapped
         let anchorEntity = AnchorEntity(anchor: anchor)
@@ -194,63 +182,145 @@ class ViewController: UIViewController {
         
         // Import the Point Charge Model, clone the entity as many times as needed
         let pointChargeAnchor = try! PointCharge.load_PointCharge()
-        let pointChargeEntity = pointChargeAnchor.pointChargeObject!
+        let pointChargeEntity = pointChargeAnchor.pointCharge!
         
         for pos in selectedPositions {
             let point = pointChargeEntity.clone(recursive: true)
             anchorEntity.addChild(point)
             point.setPosition(pos, relativeTo: anchorEntity)
             
+            // Create Text Entity for the particle
+            let textEntity = createTextEntity(pointEntity: point)
+            // Load the mesh and material for the model of the text entity
+            loadText(textEntity: textEntity, material: coulombTextMaterial, coulomb: 1)
+            
             // Install gestures
-            point.generateCollisionShapes(recursive: true)
+            point.generateCollisionShapes(recursive: false)
             arView.installGestures([.translation, .rotation, .scale], for: point as! HasCollision)
         }
         
         arView.gestureRecognizers?.forEach { recognizer in
-            // Installed gestures (EntityGesturesRecognizers for each point charge) were cancelling
-            // other touches, so turn that to false
-            recognizer.cancelsTouchesInView = false
-            
             // remove gesture recognizer for the first point of charge
             if recognizer.name == "First Point Recognizer" {
 //                arView.removeGestureRecognizer(recognizer)
                 recognizer.isEnabled = false
             }
-            // enable the point tap recognizer
-            if recognizer.name == "Point Tap Recognizer" {
+            // enable the point long tap recognizer
+            if recognizer.name == "Long Press Recognizer" {
                 recognizer.isEnabled = true
             }
+            
+            // Installed gestures (EntityGesturesRecognizers for each point charge) were cancelling
+            // other touches, so turn that to false
+            recognizer.cancelsTouchesInView = false
         }
         
     }
     
-    func loadPointChargeInfo(entity: Entity) {
-        print(entity)
-        var textModelComponent : ModelComponent = entity.children[1].children[0].children[0].components[ModelComponent] as! ModelComponent
-        textModelComponent.mesh = .generateText("2 Cb", extrusionDepth: 0, font: .systemFont(ofSize: 0.02), containerFrame: CGRect.zero, alignment: .center, lineBreakMode: .byCharWrapping)
-        entity.children[1].children[0].children[0].components.set(textModelComponent)
+    
+    
+    
+    // ---------------------------------------------------------------------------------
+    // --------------------------- TEXT ENTITY -----------------------------------------
+    func createTextEntity(pointEntity: Entity) -> Entity {
+        let textEntity: Entity = Entity()
+        textEntity.name = "text"
+        textEntity.setParent(pointEntity)
+        textEntity.setPosition(SIMD3<Float>(-0.02, -0.03, 0.03), relativeTo: pointEntity)
+
+        textEntity.setOrientation(simd_quatf(ix: -0.45, iy: 0, iz: 0, r: 0.9), relativeTo: pointEntity)
         
-        
-        
-        
+        return textEntity
     }
     
+    func loadText(textEntity: Entity, material: SimpleMaterial, coulomb: Float) {
+        let model: ModelComponent = ModelComponent(mesh: .generateText("\(coulomb) Cb",
+                                                                       extrusionDepth: 0.003,
+                                                                       font: .systemFont(ofSize: 0.02),
+                                                                       containerFrame: CGRect.zero,
+                                                                       alignment: .left,
+                                                                       lineBreakMode: .byCharWrapping),
+                                                   materials: [material])
+        textEntity.components.set(model)
+    }
+    
+    
+    
+    
+    // ---------------------------------------------------------------------------
+    // -------------------- Gesture Recognizer HANDLERS --------------------------
+    @objc func handleTap(recognizer: UITapGestureRecognizer) {
+        let location = recognizer.location(in: arView)
+        
+        let results = arView.raycast(from: location, allowing: .estimatedPlane, alignment: .horizontal)
+        
+        if let firstResult = results.first {
+            let anchor = ARAnchor(name: "PointCharge", transform: firstResult.worldTransform)
+            arView.session.add(anchor: anchor)
+        } else {
+            print("No horizontal surface found.")
+        }
+    }
+    
+    @objc func handleLongPress(recognizer: UILongPressGestureRecognizer) {
+        let location = recognizer.location(in: arView)
+        
+        guard let hitEntity = arView.entity(at: location) else {return}
+        
+        if recognizer.state == .began {
+            print("long press started")
+            
+            if hitEntity.name == "pointCharge" {
+            }
+        }
+        
+        if recognizer.state == .ended {
+            print("long press ended")
+            if hitEntity.name == "pointCharge" {
+            }
+        }
+    }
+    
+    
+    
+    
+    // ---------------------------------------------------------------------------------
+    // -------------------------- DRAG & DROP pointCharge ------------------------------
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let location = touches.first?.location(in: arView) else {return}
         guard let hitEntity = arView.entity(at: location) else {return}
         
-        if hitEntity.name == "pointChargeObject" {
+        if hitEntity.name == "pointCharge" {
             trackedEntity = hitEntity
+            
+            // Emphasize the pointCharge by scaling it up 50%
+            trackedEntity.setScale(SIMD3<Float>(3/2, 3/2, 3/2), relativeTo: trackedEntity)
+            // Hide the (text) value (coulomb) label
+            trackedEntity.children.forEach{ child in
+                if child.name == "text" {
+                    child.isEnabled = false
+                }
+            }
         }
     }
 
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        print("moved")
+        //
     }
+    
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         /// If tracked entity is a pointCharge, check if its alignment differ less than 0.05m from the other particles.
         /// If so, align it to them
-        if trackedEntity.name == "pointChargeObject" {
+        if trackedEntity.name == "pointCharge" {
+            /// De emphasize the Point Charge by scaling it down 50%
+            trackedEntity.setScale(SIMD3<Float>(2/3, 2/3, 2/3), relativeTo: trackedEntity)
+            /// Bring back the value label
+            trackedEntity.children.forEach{ child in
+                if child.name == "text" {
+                    child.isEnabled = true
+                }
+            }
+            
             let x = trackedEntity.position.x
             let z = trackedEntity.position.z
             
@@ -270,23 +340,21 @@ class ViewController: UIViewController {
                     }
                 }
             }
-            
             /// When touches end, no entity is tracked by the gesture
             trackedEntity = Entity()
         }
-
     }
     
-    // Add  Coaching View to help user
-    func overlayCoachingView() {
-        let coachingView = ARCoachingOverlayView(frame: CGRect(x: 0, y:0, width: arView.frame.width, height: arView.frame.height))
-        
-        coachingView.session = arView.session
-        coachingView.activatesAutomatically = true
-        coachingView.goal = .horizontalPlane
-        
-        view.addSubview(coachingView)
-    }
+//    // Add  Coaching View to help user
+//    func overlayCoachingView() {
+//        let coachingView = ARCoachingOverlayView(frame: CGRect(x: 0, y:0, width: arView.frame.width, height: arView.frame.height))
+//
+//        coachingView.session = arView.session
+//        coachingView.activatesAutomatically = true
+//        coachingView.goal = .horizontalPlane
+//
+//        view.addSubview(coachingView)
+//    }
     
 }
 
